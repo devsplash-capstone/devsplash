5264f098efc001e91bd4d46bca504ea95c7d3e51
dlmeredi <dlmeredi@gmail.com>
2021-09-03 11:47:21 -0500

    updated view controller, tried updating router but mitch got to it first so deleted all edits in router.js

--- src/main/java/ViewController.java
+++ src/main/java/ViewController.java
@@ -3,8 +3,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
-//    @RequestMapping({})
+    @RequestMapping({"/", "/home"})
     public String showView(){
         return "forward:/index.html";
+
     }
+
+//    private void get() {
+//    }
+// need to come up with the get() method or whatever...
+
 }
