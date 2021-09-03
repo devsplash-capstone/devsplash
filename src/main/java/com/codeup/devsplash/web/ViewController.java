3bcb9ae1b06aa293cfc4eb3886b3abe4f466d809
dlmeredi <dlmeredi@gmail.com>
2021-09-03 15:15:14 -0500

    adds errors.js, updates router.js

--- src/main/java/com/codeup/devsplash/web/ViewController.java
+++ src/main/java/com/codeup/devsplash/web/ViewController.java
@@ -1,16 +1,14 @@
+package com.codeup.devsplash.web;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
     @RequestMapping({"/", "/home"})
-    public String showView(){
+    public String showView() {
         return "forward:/index.html";
 
     }
-
-//    private void get() {
-//    }
-// need to come up with the get() method or whatever...
 
 }
