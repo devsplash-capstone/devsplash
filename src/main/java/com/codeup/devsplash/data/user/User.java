e684c51adeb85a19b8d1d59962f249955cb1ef01
PrachiPhatak <phatak.prachi@gmail.com>
2021-09-03 12:17:08 -0500

    Adds create user functionality - backend.

---
+++ src/main/java/com/codeup/devsplash/data/user/User.java
@@ -0,0 +1,89 @@
+package com.codeup.devsplash.data.user;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(nullable = false, length = 200)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String firstname;
+
+    @Column(nullable = false, length = 100)
+    private String lastname;
+
+    @Column(nullable = false, length = 100)
+    private String displayName;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String email, String firstname, String lastname, String displayName, String password) {
+        this.id = id;
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.displayName = displayName;
+        this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
