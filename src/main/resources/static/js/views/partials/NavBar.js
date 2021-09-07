a7a7961c341efee589f7d7e6eefe99b9aaf370c4
PrachiPhatak <phatak.prachi@gmail.com>
2021-09-07 18:15:52 -0500

    Adds different links to navbar if user is logged in

--- src/main/resources/static/js/views/partials/NavBar.js
+++ src/main/resources/static/js/views/partials/NavBar.js
@@ -4,10 +4,20 @@ export default function Navbar(props) {
     if(props)
         if(props.user){
             username = props.user.displayName;
-        }
-    return `
-        <nav id="nav">
-            <a class="a-tags" href="/" data-link>Home - ${username}</a>
-        </nav>
+            return `
+                    <nav id="nav">
+                        <a class="a-tags" href="/" data-link>Home</a>
+                        <a class="a-tags" href="/" data-link>${username}</a>
+                        <a class="a-tags" href="/" data-link>Sign out</a>
+                    </nav>
     `;
+        }else{
+            return `
+                    <nav id="nav">
+                         <a href="/register" data-link>Sign Up</a> |
+                         <a href="/login" data-link>Sign In</a>
+                    </nav>
+    `;
+        }
+
 }
