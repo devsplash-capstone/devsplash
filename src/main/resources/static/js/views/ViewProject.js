aaed417731275da86b0eda4f8cfca2c97ec6ccf4
dlmeredi <dlmeredi@gmail.com>
2021-09-07 16:08:06 -0500

    adds partial front-end for viewproject, still a work in progress

---
+++ src/main/resources/static/js/views/ViewProject.js
@@ -0,0 +1,12 @@
+export default function ViewProject(props) {
+    return ` 
+    <h1>My Projects Page</h1>
+    <br><br><br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dui metus, euismod vel semper a, consectetur eget neque. Suspendisse in cursus velit. Morbi fringilla imperdiet tortor, eget maximus dui tristique sit amet. Etiam a pellentesque risus. Mauris elementum semper sem vel maximus. Maecenas imperdiet sem cursus leo tincidunt tempus. Aliquam ut quam nisi. Nunc cursus ac lorem eget imperdiet. Suspendisse velit risus, aliquam ac rutrum et, blandit in leo. Etiam dictum egestas enim, in posuere lorem accumsan a. Aenean luctus leo ut ullamcorper pulvinar. Sed ut mi venenatis, vestibulum nisl non, interdum magna. Pellentesque eu mi mauris. Aliquam nunc ante, eleifend sit amet libero in, convallis porta leo. Sed dolor dui, laoreet nec aliquet at, consequat eget quam.  
+</p>
+    
+<!-- STILL WORKING ON FRONT END PORTION OF VIEWPROJECT! HOPEFULLY I CAN FINISH THE REST BY TONIGHT!   -->
+    
+`
+
+};
