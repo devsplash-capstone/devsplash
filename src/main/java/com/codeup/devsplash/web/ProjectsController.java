96b2557f2776ec0933b54d03b09e307ea6d893c0
PrachiPhatak <phatak.prachi@gmail.com>
2021-09-08 17:07:44 -0500

    Adds create project - frontend

--- src/main/java/com/codeup/devsplash/web/ProjectsController.java
+++ src/main/java/com/codeup/devsplash/web/ProjectsController.java
@@ -2,23 +2,19 @@ package com.codeup.devsplash.web;
 
 import com.codeup.devsplash.data.project.Project;
 import com.codeup.devsplash.data.project.ProjectsRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value="api/project", headers = "Accepts=application/json")
+@RequestMapping(value="api/projects", headers = "Accept=application/json")
 public class ProjectsController {
 
     private final ProjectsRepository projectsRepository;
-
 
     public ProjectsController(ProjectsRepository projectsRepository) {
         this.projectsRepository = projectsRepository;
     }
 
-    @PostMapping("/create")
+    @PostMapping
     private void createProject(@RequestBody Project newProject){
         projectsRepository.save(newProject);
     }
--- src/main/resources/static/js/views/Profile.js
+++ src/main/resources/static/js/views/Profile.js
@@ -2,14 +2,13 @@ import createView from "../createView.js";
 
 export default function Profile(props) {
     console.log(props.user.error)
-    if(props.user.error)
-    {
+    if (props.user.error) {
         console.log("Not a member - Login / Signup")
         createView("/login");
     }
 
-
-    return `<h1>Welcome ${props.user.displayName}!</h1>`
+    return `<h1>Welcome ${props.user.displayName}!</h1>
+            <a href="/project" data-link>Create Project</a>`
 }
 
 export function ProfileEvent() {
