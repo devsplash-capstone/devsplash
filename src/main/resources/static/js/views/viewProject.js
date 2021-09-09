f19b2b2142edab5b161e5b54d0e3f5b521988808
dlmeredi <dlmeredi@gmail.com>
2021-09-09 16:08:14 -0500

    still editing to add project data to display onto webpage

--- src/main/resources/static/js/views/viewProject.js
+++ src/main/resources/static/js/views/viewProject.js
@@ -3,22 +3,21 @@ export default function ViewProject(props) {
  <!DOCTYPE html>
  <html lang="eng">
 <head> 
-<meta charset="UTF-8"/>
+<meta charset="utf-8"/>
+<meta name="viewport" content=""
 <title>My Projects Page</title>
 </head>
 <br>
 <br>
    <body>
-   <h1>Project Description</h1>
+   <h1>PROJECT NAME HERE</h1>
+   <br>
+  
    <br>
    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dui metus, euismod vel semper a, consectetur eget neque. Suspendisse in cursus velit. Morbi fringilla imperdiet tortor, eget maximus dui tristique sit amet. Etiam a pellentesque risus. Mauris elementum semper sem vel maximus. Maecenas imperdiet sem cursus leo tincidunt tempus. Aliquam ut quam nisi. Nunc cursus ac lorem eget imperdiet. Suspendisse velit risus, aliquam ac rutrum et, blandit in leo. Etiam dictum egestas enim, in posuere lorem accumsan a. Aenean luctus leo ut ullamcorper pulvinar. Sed ut mi venenatis, vestibulum nisl non, interdum magna. Pellentesque eu mi mauris. Aliquam nunc ante, eleifend sit amet libero in, convallis porta leo. Sed dolor dui, laoreet nec aliquet at, consequat eget quam.  
 </p>
 <br>
-<br>
-<h3>PLACEHOLDER FOR GITHUB/GITLAB PREVIEW + INFO ABOUT PROJECT</h3>
-<br>
-<br>
-<h3>LIST MEMBERS ASSOCIATED WITH PROJECT</h3>
+<h3>LIST MEMBERS ASSOCIATED WITH PROJECT (possibly with their icons or just their usernames? to be determined later...)</h3>
 <br>
 <br>
 <button type="submit" id="edit-btn">Edit</button>
@@ -30,7 +29,6 @@ export default function ViewProject(props) {
 export function ViewProjectEvent() {
     ViewProjectEdit();
 }
-
 
 function ViewProjectEdit() {
     //TODO: pass in ID
@@ -44,8 +42,8 @@ function ViewProjectEdit() {
         fetch(`http://localhost:8080/api/projects/findById/${id}`, request).then(
             (response) => {
                 console.log(response);
-
-            });
+                // display(id)
+            })
     })
 }
 
