9d182b544b7ce74b1b94268fabb7eece90642767
dlmeredi <dlmeredi@gmail.com>
2021-09-16 10:07:09 -0500

    adds update to router.js

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -4,12 +4,12 @@ import Error404 from "./views/errors.js";
 import Login from "./views/Login.js";
 import LoginEvent, {LogoutEvent} from "./auth.js";
 import Register, {RegisterEvent} from "./views/Register.js";
-<<<<<<< HEAD
+
 import Profile, {ProfileEvent} from "./views/Profile.js";
 import ProjectComponent, {ViewProjects} from "./views/Project.js";
 import Projects from "./views/Projects.js";
 import Members from "./views/Members.js";
-=======
+
 import ProfileView, {ProfileEvent} from "./views/Profile.js";
 import ViewProject, {ViewProjectEvents} from "./views/viewProject.js";
 import ProjectComponent, {EditProjectEvent} from "./views/EditProject.js";
@@ -17,7 +17,7 @@ import ProjectsView, {ProjectsViewEvents} from "./views/Projects.js";
 import Members, {MembersEvent} from "./views/Members.js";
 import EditProfile, {EditProfileEvent} from "./views/EditProfile.js";
 import EditProjectView from "./views/EditProject.js";
->>>>>>> 5805c9b7c45b7c9a645e73223e4ff686ed89423f
+
 
 /**
  * Returns the route object for a specific route based on the given URI
@@ -92,11 +92,11 @@ export default function router(URI) {
             },
             uri: '/project',
             title: "Project",
-<<<<<<< HEAD
-            viewEvent: ViewProjects
-=======
+
+            // viewEvent: ViewProjects,
+
             viewEvent: EditProjectEvent
->>>>>>> 5805c9b7c45b7c9a645e73223e4ff686ed89423f
+
         },
         '/projects': {
             returnView: ProjectsView,
