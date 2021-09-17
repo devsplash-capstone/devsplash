53dd2c96bbb0deee12f480063244fb3e7b977d6e
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-09-17 12:26:36 -0500

    commits landing page rework

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -1,77 +1,71 @@
 export default function Home(props) {
     return `
+        <body style="padding: 3.5rem">
         <div id="app" class="container p-2">
-            <header>
-                <div id="title">
-                    <div class="col-12 text-center">
-                        <h1>Combine your skills with others, today</h1>
-                    </div>
+           <main role="main">
+
+            <!-- START THE FEATURETTES -->
+            <div class="row featurette mt-5 pt-5 align-self-center">
+                <h3>Already a member?</h3>
+                <div class="ml-2">
+                    <form action="DevsplashMockup/profile.html">
+                        <button class="btn btn-secondary">Log in</button>
+                    </form>
                 </div>
-                <div class="text-center">
-                            <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="photoLead" width="350px" class="rounded">
-                            </div>
-            </header>
-            <main>
-                <div class="text-center">
-                    <div id="about" class="row">
-                        <div class="col-12">
-                            <h3>What is DevSplash?</h3>
-                        </div>
-                        <br>
-                        <br>
-                        <div class="col-12">
-                            <p>
-                            Devsplash is an application for application creators. Devsplash is used to pair up 
-                            programmers with a variety of skills to which they can combine their knowledge and create 
-                            something in which neither of them could do on their own. 
-                            </p>
-                        </div>
+            </div>
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">Devsplash brings fresh ideas to life
+                        <span class="text-muted">by matching skills with project
+                        needs.</span>
+                    </h2>
+                    <p class="lead">Members can use their background and skill sets to match with available
+                        projects.</p>
+                    <div class="m-2">
+                        <form action="DevsplashMockup/signup.html">
+                            <button class="btn btn-secondary">Sign up!</button>
+                        </form>
                     </div>
-                    <div id="works">
-                        <hr>
-                        <h3>How does DevSplash work?</h3>
-                         <div class="text-center">
-                                    <img src="https://envato-shoebox-0.imgix.net/5817/5f40-ebdb-48d5-b950-e889f3c4d37e/IMG_3992.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=173fc01bad4a57ea116f57088c39d29a" alt="stockOne" width="300px" class="rounded">
-                         </div>
-                        <h4 id="becoming-member">1. Become a member </h4>
-                        <br>
-                        <div>
-                            <p>
-                            Sign up for free! We look over every application to ensure that the community can rely on 
-                            the skillset you can provide
-                            </p>
-                        </div>
-                        <br>
-                        <div class="text-center">
-                        <img src="https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562?k=20&m=1075599562&s=612x612&w=0&h=cDFY2kKyhFzSNNlDQsaxoekIW0v7iyaMBkxp11Fz33U=" alt="stockTwo" width="300px" class="rounded">
-                        </div>
-                        <h4 id="create-project">2. Create a project or find a team</h4>
-                        <div>
-                            <p class="">
-                            Once you're signed up, Devsplash makes it easy for you to start looking for certain 
-                            languages you need. Or if you don't have a project in mind, you can start exploring the open
-                             projects and see if you're the right fit for any!
-                            </p>
-                        </div>
-                        <br>
-                        <div>
-                         <div class="text-center">
-                        <img src="https://www.mheducation.com/content/dam/mhe/ideas/new/ditch-critical-thinking.jpg" alt="stockThree" width="300px"                                 class="rounded">
-                        </div>
-                            <div>
-                                <h4 id="start-planning">3. Start planning!</h4>
-                            </div>
-                            <div>
-                                <p class="">
-                                Devsplash provides a multitude of ways to communicate to your partner. Whether 
-                                email, directly message, or if you want to exchange numbers. We understand the 
-                                importance of communication to your team, and try and make it as easy as possible.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
-            </main>
-        </div>
+                <div class="col-md-5">
+                    <img class="featurette-image img-fluid mx-auto" src="https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562?k=20&m=1075599562&s=612x612&w=0&h=cDFY2kKyhFzSNNlDQsaxoekIW0v7iyaMBkxp11Fz33U="
+                         alt="Generic placeholder image">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">Meet the like-minded. <span
+                            class="text-muted">See for yourself.</span></h2>
+                    <p class="lead">Members can find and introduce themselves to potential connections,
+                        with the goal of collaborating on projects or learning new skills.</p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img class="featurette-image img-fluid mx-auto" src="https://envato-shoebox-0.imgix.net/5817/5f40-ebdb-48d5-b950-e889f3c4d37e/IMG_3992.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=173fc01bad4a57ea116f57088c39d29a"
+                         alt="Generic placeholder image">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">Collaborate and create. <span class="text-muted">Checkmate.</span>
+                    </h2>
+                    <p class="lead">Devsplash does match-making based on shared interests, location & technology
+                        stack.</p>
+                </div>
+                <div class="col-md-5">
+                    <img class="featurette-image img-fluid mx-auto" src="https://www.mheducation.com/content/dam/mhe/ideas/new/ditch-critical-thinking.jpg"
+                         alt="Generic placeholder image">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
     `;
 }
