d7deb15ad48c024060bf219a826ebb6b62a01376
dlmeredi <dlmeredi@gmail.com>
2021-09-23 13:52:09 -0500

    adds todos

--- src/main/resources/static/js/views/Faq.js
+++ src/main/resources/static/js/views/Faq.js
@@ -1,5 +1,10 @@
 export default function Faq(props){
-    return `
+/* TODO: add 10-15 standard faq questions
+    create wireframe for UI
+    add UI (keep uniform with rest of site!)
+    update router.js for faq
+ */
+ return `
     
     
     
