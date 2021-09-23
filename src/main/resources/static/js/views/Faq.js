7b92f027ff835346bc1426f095ae76a96833921f
dlmeredi <dlmeredi@gmail.com>
2021-09-23 14:26:22 -0500

    adds questions

--- src/main/resources/static/js/views/Faq.js
+++ src/main/resources/static/js/views/Faq.js
@@ -4,6 +4,22 @@ export default function Faq(props){
     add UI (keep uniform with rest of site!)
     update router.js for faq
  */
+
+    /* TODO: FAQ QUESTIONS!!!!
+        1. What is Devsplash and how does it work?
+        2. How do I join a project?
+        3. Do I have to pay to use Devsplash?
+        4. How do I know if I have been accepted to a project?
+        5. Why was I not accepted for a project that I requested to join?
+        6. How do I message other members within Devsplash?
+        7. How do I contact the administrators?
+        8.  ...
+        9.  ...
+        10. ...
+
+** goal is 10 questions for now... will adjust, add more as we see fit!
+
+     */
  return `
     
     
