1a3208619f3e03c03063b1be52148621046830eb
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-09-27 16:43:54 -0500

    adds modal effect to edit project and about us page

--- src/main/java/com/codeup/devsplash/web/ViewController.java
+++ src/main/java/com/codeup/devsplash/web/ViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
-    @RequestMapping({"/", "/home","/register","/profile", "/login", "/viewproject", "/projects", "/members"})
+    @RequestMapping({"/", "/home","/register","/profile", "/login", "/viewproject", "/projects", "/members", "/aboutus"})
     public String showView() {
         return "forward:/index.html";
     }
