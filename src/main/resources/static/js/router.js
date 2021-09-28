574e404962d141de2a5ffdb60e09c05bd195ef5d
dlmeredi <dlmeredi@gmail.com>
2021-09-28 10:08:07 -0500

    okay THIS resolves the merge conflicts

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -9,13 +9,10 @@ import EditProjectComponent, {EditProjectEvents} from "./views/EditProject.js";
 import ProjectsView, {ProjectsViewEvents} from "./views/Projects.js";
 import Members, {MembersEvents} from "./views/Members.js";
 import EditProfile, {EditProfileEvent} from "./views/EditProfile.js";
-<<<<<<< HEAD
 import ProjectView, {ProjectEvents} from "./views/Project.js";
 import PrivacyPolicy from "./views/PrivacyPolicy.js";
-=======
 import ProjectView from "./views/Project.js";
 import AboutUsView from "./views/AboutUs.js";
->>>>>>> 23c7e6efbbc6933685883152cd63b2293b86aa8b
 
 export function validateUser(obj) {
     if (localStorage.getItem("access_token")){
